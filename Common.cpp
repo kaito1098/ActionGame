08b477a64dc42ae16e7c812edb4d21fb1103ad02
kaito1098 <107204284+kaito1098@users.noreply.github.com>
2022-07-08 14:21:03 +0900

    不要ファイル整理

--- Common.cpp
+++ Common.cpp
@@ -1,13 +1,9 @@
 #include "Common.h"
 
 std::unique_ptr<GameManager> gameManagerPtr = std::make_unique<GameManager>();
-//・当たり判定制御用管理クラス
-std::unique_ptr<ColliderManager> colliderManager = std::make_unique<ColliderManager>();
 
 const int SCREEN_WIDTH = 1600;
 const int SCREEN_HEIGHT = 900;
-//・当たり判定分割レベル
-const int COLLIDERS_TREE_LEVEL = 3;
 
 const int MAX_FALL_SPEED = 8;
 
